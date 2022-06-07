0251a10bdd14cc2b2d2f0611718adfb7b99aa011
NurRizqiMaulana <maulana29.rizqi@gmail.com>
2022-06-07 18:43:28 +0700

    first commit

---
+++ src/Bubblesort/Bubblesort.java
@@ -0,0 +1,41 @@
+package Bubblesort;
+
+public class Bubblesort {
+    private String [] data;
+
+    //    constructor
+        public Bubblesort(){
+    
+        }
+        public void setData (String[] data){
+            this.data=data;
+        }
+        public void sortData(){
+            int lenData = this.data.length;
+            for(int i = 0; i<lenData;i++){
+                for(int j=i+1;j<lenData;j++){
+                    if (this.data[j].compareTo(this.data[i])>0){
+                        String temp = this.data[i];
+                        this.data[i]=this.data[j];
+                        this.data[j]=temp;
+                    }
+                }
+            }
+        }
+        public void PrintData(){
+            for (String datum:this.data){
+                System.out.println(datum+"");
+            }
+        }
+    
+        public static void main(String[] args) {
+    //        int[] s = new int[]{10,35,32,13,26};
+            String [] nama={"spiderman","superman","batman","ant man","wonderwoman","gundala","thor"};
+            Bubblesort _myBubble = new Bubblesort();
+            _myBubble.setData(nama);
+            _myBubble.sortData();
+            _myBubble.PrintData();
+        }
+    
+    
+}
